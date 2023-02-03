e73a2ad45d3cec00476c905c3c02c5f32ed24597
Muhammed Yasir Özdemir <yasiryksmail@gmail.com>
2023-02-03 12:39:58 +0300

    navbar done

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -1,10 +1,49 @@
 import { Component } from "react";
+import { Navbar, Nav, Dropdown, NavDropdown } from "react-bootstrap";
+import navLogo from "../assets/netflix_logo.png";
+import navIcon from "../assets/avatar.png";
 
 class CustomNavbar extends Component {
   render() {
     return (
       <>
-        <section className="container-fluid"></section>
+        <Navbar variant="dark" className="text-white" expand="lg">
+          <Navbar.Brand href="#">
+            <img className="navLogo w-100" src={navLogo} alt="logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+            </Nav>
+            <Nav className="ml-auto d-flex align-items-lg-center">
+              <Nav.Link href="#home">Search</Nav.Link>
+              <Nav.Link href="#home">KIDS</Nav.Link>
+              <Nav.Link href="#home">Notifications</Nav.Link>
+              <NavDropdown
+                variant="outline-secondary"
+                alignRight
+                title={
+                  <img
+                    className="navIcon image-fluid"
+                    src={navIcon}
+                    alt="navIcon"
+                  />
+                }
+                id="navDropdown"
+              >
+                <Dropdown.Item
+                  className="border-bottom border-dark"
+                  eventKey="1"
+                >
+                  Back Office
+                </Dropdown.Item>
+                <Dropdown.Item eventKey="2">Account</Dropdown.Item>
+                <Dropdown.Item eventKey="3">Edit Profile</Dropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       </>
     );
   }
